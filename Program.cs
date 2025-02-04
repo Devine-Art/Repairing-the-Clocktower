ddf05cd80cd6cd367dde5ba95a6ef9c98e6e943f
Devine-Art <adevine149@gmail.com>
2025-02-04 17:19:40 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace Repairing_the_Clocktower
+{
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        //ask user for variable and assign it
+        Console.WriteLine("Enter #");
+        int tickTock = int.Parse(Console.ReadLine());
+
+        //statement to determine output
+        if (tickTock % 2 == 0) ;
+        {
+            //This is an even output
+            Console.WriteLine("Tick");
+        }
+
+        else
+        {
+            //this is an odd output
+            Console.WriteLine("Tock");
+        }
+
+    }
+}
+}
